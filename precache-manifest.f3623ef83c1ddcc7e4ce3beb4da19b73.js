c4572e02e481b3745a488eb704630516bc40c567
Bogdan Mykolaichuk <bogmw.it@gmail.com>
2020-09-12 19:06:29 +0300

    Updates

---
+++ precache-manifest.f3623ef83c1ddcc7e4ce3beb4da19b73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8887a0aff5dcdbde5c91b4fc0b48d2f9",
+    "url": "/ReactReduxTypescripSaga/index.html"
+  },
+  {
+    "revision": "1a0e3c77ed05ae75f59a",
+    "url": "/ReactReduxTypescripSaga/static/css/2.deb2c4ab.chunk.css"
+  },
+  {
+    "revision": "9e11f3866f47f9465eed",
+    "url": "/ReactReduxTypescripSaga/static/css/main.c7ccb357.chunk.css"
+  },
+  {
+    "revision": "1a0e3c77ed05ae75f59a",
+    "url": "/ReactReduxTypescripSaga/static/js/2.0ac326e5.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/ReactReduxTypescripSaga/static/js/2.0ac326e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e11f3866f47f9465eed",
+    "url": "/ReactReduxTypescripSaga/static/js/main.cf23cb84.chunk.js"
+  },
+  {
+    "revision": "9626634c46bafcfb81c9",
+    "url": "/ReactReduxTypescripSaga/static/js/runtime-main.126fa6bd.js"
+  }
+]);
